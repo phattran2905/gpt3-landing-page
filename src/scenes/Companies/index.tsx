d054f8a28ef6872bd07154a68274f78447153020
phat@minim1 <phat.tranducvn@gmail.com>
2023-05-15 19:29:11 -0700

    Fix typo file's name

--- src/scenes/Companies/index.tsx
+++ src/scenes/Companies/index.tsx
@@ -1,6 +1,6 @@
 import "./index.css"
 import Google from '@assets/google.png';
-import Slack from '@assets/Slack.png';
+import Slack from '@assets/slack.png';
 import Atlassian from '@assets/atlassian.png';
 import Dropbox from '@assets/dropbox.png';
 import Shopify from '@assets/shopify.png';
